ed7f4117fe228b51e06de1ba323d34ad8446b404
Tammy Freire <tammy.freire@gmail.com>
2023-03-14 17:10:17 +0100

    Add account holders create endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import healthCheckRoutes from "./routes/health_check";
 import { json } from "body-parser";
+import accountHoldersRoutes from "./routes/account_holder";
 
 const app = express();
 app.use(json());
 
 app.use("/api/v1/health-check", healthCheckRoutes);
+app.use("/api/v1/account_holders", accountHoldersRoutes);
 
 export default app;
