b234b0d661e8d8e2f77df2d2cbd7d3a7c5204eae
Tammy Freire <tammy.freire@gmail.com>
2023-03-16 15:13:05 +0100

    Fix table naming to lower case to eliminate the need of quotes on db querries

--- prisma/migrations/20230316140127_init/migration.sql
+++ prisma/migrations/20230316140127_init/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
-CREATE TABLE "AccountHolder" (
+CREATE TABLE "account_holder" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "age" INTEGER NOT NULL,
 
-    CONSTRAINT "AccountHolder_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "account_holder_pkey" PRIMARY KEY ("id")
 );
