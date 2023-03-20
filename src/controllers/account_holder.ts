8938e3c9d76e96bde2ab1dc15ced0b03337e3626
Tammy Freire <tammy.freire@gmail.com>
2023-03-20 18:33:03 +0100

    add support for multiple errors on validation

--- src/controllers/account_holder.ts
+++ src/controllers/account_holder.ts
@@ -1,12 +1,21 @@
 import { RequestHandler } from "express";
 import client from "../client";
+import { AccountHolder } from "../models/account_holder";
 
 export const create: RequestHandler = async (req, res, _next) => {
   const name: string = req.body.name;
-  const age: number = req.body.age;
+  const age: number = +req.body.age;
 
-  const account_holder = await client.account_holder.create({
-    data: { name, age },
-  });
-  res.status(200).json(account_holder);
+  let account_holder = new AccountHolder(name, age);
+
+  if (account_holder.validate()) {
+    const account_holder = await client.account_holder.create({
+      data: { name, age },
+    });
+    res.status(200).json(account_holder);
+  } else {
+    res.status(400).json({
+      errors: account_holder.errors,
+    });
+  }
 };
--- test/models/account_holder.test.ts
+++ test/models/account_holder.test.ts
@@ -10,23 +10,67 @@ describe("account holder class", () => {
     });
   });
 
-  describe("#is_valid", () => {
-    test("returns true when it's valid", () => {
+  describe("#validate", () => {
+    test("returns true when valid", () => {
       const account_holder = new AccountHolder("John", 87);
 
-      expect(account_holder.is_valid()).toBe(true);
+      expect(account_holder.validate()).toBe(true);
     });
 
-    test("returns false when name is invalid", () => {
+    test("returns false when name is an empty string", () => {
       const account_holder = new AccountHolder("", 87);
 
-      expect(account_holder.is_valid()).toBe(false);
+      expect(account_holder.validate()).toBe(false);
     });
 
-    test("returns false when age is invalid", () => {
+    test("returns false when age is less than 0", () => {
       const account_holder = new AccountHolder("John", -17);
 
-      expect(account_holder.is_valid()).toBe(false);
+      expect(account_holder.validate()).toBe(false);
     });
+    // test("returns false when age is empty string", () => {
+    //   const account_holder = new AccountHolder("John", "");
+
+    //   expect().toBe(false);
+    // });
   });
+  describe("#errors", () => {
+    test("populate errors array upon invalid results", () => {
+      const account_holder = new AccountHolder("John", -17);
+
+      let result = account_holder.validate();
+
+      expect(result).toBe(false);
+
+      expect(account_holder.errors).toEqual([
+        { message: "cannot be less than 0", field: "age" },
+      ]);
+    }),
+      test("in case results are valid there should be no errors", () => {
+        const account_holder = new AccountHolder("John", 87);
+
+        let result = account_holder.validate();
+        expect(result).toBe(true);
+
+        expect(account_holder.errors).toEqual([]);
+      });
+  }),
+    test("in case multiple fields are invalid there should be multiple errors", () => {
+      const account_holder = new AccountHolder("", -17);
+
+      let result = account_holder.validate();
+
+      expect(result).toBe(false);
+
+      expect(account_holder.errors).toEqual([
+        {
+          message: "cannot be less than 0",
+          field: "age",
+        },
+        {
+          message: "cannot be blank",
+          field: "name",
+        },
+      ]);
+    });
 });
