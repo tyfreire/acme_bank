97fcccc0f006942fbc016d7298bac7dcd2b18be7
Tammy Freire <tammy.freire@gmail.com>
2023-03-27 17:09:43 +0200

    Store bank account into DB

--- src/controllers/bank_account.ts
+++ src/controllers/bank_account.ts
@@ -4,15 +4,13 @@ import { BankAccount } from "../models/bank_account";
 
 export const create: RequestHandler = async (req, res, _next) => {
   const account_holder_id: number = +req.body.account_holder_id;
-  const type: string = req.body.type;
+  const type = req.body.type;
 
   let bank_account = new BankAccount(account_holder_id, type);
 
-  const bank_account = await client.bank_account.create({
-    data: {
-      account_holder_id,
-      type,
-    },
+  const bank_account_1 = await client.bank_account.create({
+    data: bank_account,
   });
-  res.status(200).json(bank_account);
+
+  res.status(200).json(bank_account_1);
 };
