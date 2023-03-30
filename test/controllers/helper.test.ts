af1237ca85773ca9fa6aec84e3d19415d5e53d4e
Tammy Freire <tammy.freire@gmail.com>
2023-03-30 17:17:55 +0200

    Implement last test for bank account create endpoint

--- test/controllers/helper.test.ts
+++ test/controllers/helper.test.ts
@@ -1,7 +1,21 @@
+import { bank_account_status, bank_account_type } from "@prisma/client";
 import client from "../../src/client";
 
 export async function insert_account_holder(name: string, age: number) {
   return await client.account_holder.create({
     data: { name, age },
+  });
+}
+
+export async function insert_bank_account(
+  account_holder_id: number,
+  bank_account_type: bank_account_type
+) {
+  return await client.bank_account.create({
+    data: {
+      account_holder_id: account_holder_id,
+      type: bank_account_type,
+      status: bank_account_status.OPEN,
+    },
   });
 }
