1a251386ed514cd12486b0dbad58f46cfd7699c4
Tammy Freire <tammy.freire@gmail.com>
2023-05-02 15:33:49 +0200

    Implements delete endpoint

--- src/controllers/bank_account.ts
+++ src/controllers/bank_account.ts
@@ -1,12 +1,12 @@
 import { RequestHandler } from "express";
 import client from "../client";
-import { BankAccount } from "../models/bank_account";
+import { BankAccount, update_to_close } from "../models/bank_account";
 
 export const create: RequestHandler = async (req, res, _next) => {
   const account_holder_id: number = +req.body.account_holder_id;
   const type = req.body.type;
 
-  let bank_account = new BankAccount(account_holder_id, type);
+  const bank_account = new BankAccount(account_holder_id, type);
 
   if (await bank_account.validate()) {
     const bank_account_1 = await client.bank_account.create({
@@ -37,6 +37,26 @@ export const show: RequestHandler = async (req, res, _next) => {
 
   if (bank_account) {
     res.status(200).json(bank_account);
+  } else {
+    res.status(404).json({
+      errors: [
+        {
+          field: "id",
+          message: "Bank account could not be found",
+        },
+      ],
+    });
+  }
+};
+
+export const destroy: RequestHandler = async (req, res, _next) => {
+  const id: number = +req.params.id;
+
+  const result = await client.bank_account.findUnique({ where: { id: id } });
+
+  if (result) {
+    const close_bank_account = await update_to_close(id);
+    res.status(200).json(close_bank_account);
   } else {
     res.status(404).json({
       errors: [
